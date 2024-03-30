c518575775fe61a39ae61ac8fc71d9607c798eda
Mendaliz <56089348+Mendaliz@users.noreply.github.com>
2024-03-30 23:23:34 +0300

    Add files via upload

---
+++ data/users_api.py
@@ -0,0 +1,101 @@
+import flask
+from flask import jsonify, make_response, request
+from werkzeug.security import generate_password_hash
+
+from . import db_session
+from .users import User
+
+blueprint = flask.Blueprint(
+    'users_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/users')
+def get_users():
+    db_sess = db_session.create_session()
+    users = db_sess.query(User).all()
+    return jsonify(
+        {
+            'user':
+                [item.to_dict(only=('surname', 'name', 'position', 'email'))
+                 for item in users]
+        }
+    )
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    db_sess = db_session.create_session()
+    users = db_sess.query(User).get(user_id)
+    if not users:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(
+        {
+            'user': users.to_dict(only=(
+                'surname', 'name', 'age', 'position', 'speciality',
+                'address', 'email', 'hashed_password'))
+        }
+    )
+
+
+@blueprint.route('/api/users', methods=['POST'])
+def create_user():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['surname', 'name', 'age', 'position', 'speciality',
+                  'address', 'email', 'hashed_password']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    user = User(
+        surname=request.json['surname'],
+        name=request.json['name'],
+        age=request.json['age'],
+        position=request.json['position'],
+        speciality=request.json['speciality'],
+        address=request.json['address'],
+        email=request.json['email']
+    )
+    user.set_password(request.json['hashed_password'])
+    db_sess.add(user)
+    db_sess.commit()
+    return jsonify({'id': user.id})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).get(user_id)
+    if not user:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    db_sess.delete(user)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['PUT'])
+def edit_user(user_id):
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['surname', 'name', 'age', 'position', 'speciality',
+                  'address', 'email', 'hashed_password']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+
+    user = db_sess.query(User).get(user_id)
+
+    user.surname = request.json['surname']
+    user.name = request.json['name']
+    user.age = request.json['age']
+    user.position = request.json['position']
+    user.speciality = request.json['speciality']
+    user.address = request.json['address']
+    user.email = request.json['email']
+    user.hashed_password = generate_password_hash(request.json['hashed_password'])
+
+    db_sess.commit()
+    return jsonify({'id': user.id})
+
